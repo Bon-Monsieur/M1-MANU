17876345abdff0c4fcc8b55928d02f3d17ef5e82
RaphBi <2raphael.bigey@gmail.com>
2024-11-05 16:15:07 +0100

    ajout debut tp3.py

--- analyse_num1/tp3.py
+++ analyse_num1/tp3.py
@@ -12,33 +12,45 @@ nint = 20
 h = (b-a)/nint
 x = np.arange(a,b+h,h)
 
-
+#print(x)
+#print(len(x))
 
 def poisson1D(x,f):
     
-    A = -2*np.identity(len(x)-2)
+    A = -2*np.identity(len(x)-1)
     b = [1]*(len(x)-2)
     np.fill_diagonal(A[1:],b)
     np.fill_diagonal(A[:,1:],b)
+    A[0,0] = -3/2
+    A[0,1] = 2
+    A[0,2] = -1/2
     
     A = A*(1/h**2)
+    #print(A)
     
-    F = np.array([f(x[i]) for i in range(1,len(x)-1)])
-   
-    Bc = np.zeros(len(x)-2)
-    Bc[0] = alpha/h**2
-    Bc[-1] = beta/h**2
+    F = [0]
+    for i in range(1,len(x)-1):
+        F.append(f(x[i]))
+    #print(len(F))
     
-    Uh = np.dot(np.linalg.inv(A),(F+Bc))
-    
-    Uh = np.insert(Uh, 0, alpha)
-    Uh = np.append(Uh, beta)
-    
+    Bc = np.zeros(len(x)-1)
+    Bc[0] = alpha/h
+    Bc[-1] = -beta/h**2
 
+    Uh = np.linalg.inv(A).dot(F+Bc)
+    Uh = np.append(Uh,beta)
+    print(Uh)
     return Uh
 
 
 Uh = poisson1D(x,np.exp)
-plt.scatter(x,Uh,marker='.')
+
+
+# Création du graphique
+plt.plot(x, Uh, marker='.') 
+plt.title("Solution numérique avec 21 points")
+
+# Affichage
+plt.show()
 
 # %%
