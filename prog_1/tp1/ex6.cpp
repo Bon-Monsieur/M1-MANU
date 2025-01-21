9d714f909c85cca8b53f5c40aa86d6a0162f46f6
RaphBi <2raphael.bigey@gmail.com>
2025-01-21 18:59:37 +0100

    ajout ex6

---
+++ prog_1/tp1/ex6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void CalculateRealAndImaginary(double r, double theta, double* pReal, double* PImaginary){
+    
+}
+
+
+
+int main(){
+
+    return 0;
+}
