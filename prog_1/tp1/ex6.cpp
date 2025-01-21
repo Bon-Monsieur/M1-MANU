7602155fbaab5b68f61f4235aa4e4ff9366aee76
RaphBi <2raphael.bigey@gmail.com>
2025-01-21 20:29:09 +0100

    fin ex6

--- prog_1/tp1/ex6.cpp
+++ prog_1/tp1/ex6.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void CalculateRealAndImaginary(double r, double theta, double* pReal, double* PImaginary){
-    
+
+
+void CalculateRealAndImaginary(double r, double theta, double* pReal, double* pImaginary) {
+    *pReal = r * cos(theta);        
+    *pImaginary = r * sin(theta);   
 }
 
 
 
-int main(){
+int main() {
+    double theta = M_PI / 3;
+    double r = 1;
+
+    double pReal = 0;
+    double pImaginary = 0;
+
+    double* pointReal = &pReal;  
+    double* pointImag = &pImaginary;
+
+    // Appel de la fonction avec les adresses
+    CalculateRealAndImaginary(r, theta, pointReal, pointImag);
+
+    // Affichage des résultats
+    cout << "Real part: " << pReal << " Imaginary part: " << pImaginary << endl;
 
     return 0;
 }
