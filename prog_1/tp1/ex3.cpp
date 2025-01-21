ddb63acbed3bd07ffaf58aba3d6cc40f73849bd4
RaphBi <2raphael.bigey@gmail.com>
2025-01-21 20:05:38 +0100

    correction ex3

--- prog_1/tp1/ex3.cpp
+++ prog_1/tp1/ex3.cpp
@@ -7,13 +7,25 @@ int main(){
 
     int n = 5;
     int triangle[n][n];
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            triangle[i][j] = 0;
+        }
+    }
     triangle[0][0]=1;
     
-    for (int i=0;i<n;i++){
+    for (int i=1;i<n;i++){
         triangle[i][0] = 1;
 
         for (int j=1;j<n;j++){
-            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+            if (j<=i){
+                 triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+            }
+            else{
+                triangle[i][j]=0;
+            }
+           
         }
     }
 
